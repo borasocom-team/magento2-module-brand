d04e24eae2dd8f4222946142b1f59d9531eae224
Fabio Brunelli <arioch1984@gmail.com>
2018-12-13 17:47:24 +0100

    added brand image

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -6,16 +6,25 @@ use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
+use Dmatthew\Brand\Setup\BrandSetupFactory;
 
 class UpgradeData implements UpgradeDataInterface
 {
 
+    protected $eavSetupFactory;
+
+    protected $brandSetupFactory;
+
     /**
      * @param EavSetupFactory $eavSetupFactory
      */
-    public function __construct(EavSetupFactory $eavSetupFactory)
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        BrandSetupFactory $brandSetupFactory
+    )
     {
         $this->eavSetupFactory = $eavSetupFactory;
+        $this->brandSetupFactory = $brandSetupFactory;
     }
 
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
@@ -24,6 +33,7 @@ class UpgradeData implements UpgradeDataInterface
 
         /** @var EavSetup $eavSetup */
         $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $brandSetup = $this->brandSetupFactory->create(['setup' => $setup]);
 
         if (version_compare($context->getVersion(), '1.0.1', '<=')) {
             $eavSetup->addAttribute(
@@ -49,6 +59,23 @@ class UpgradeData implements UpgradeDataInterface
                     'unique'                  => false
                 ]
             );
+
+            if (version_compare($context->getVersion(), '1.2.0', '<=')) {
+                $brandSetup->addAttribute(
+                    \Dmatthew\Brand\Model\Brand::ENTITY,
+                    'brand_image',
+                    [
+                        'type' => 'varchar',
+                        'label' => 'Custom Image',
+                        'input' => 'image',
+                        'backend' => 'Dmatthew\Brand\Model\Brand\Attribute\Backend\Image',
+                        'required' => false,
+                        'sort_order' => 9,
+                        'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                        'group' => 'General Information',
+                    ]
+                );
+            }
         }
 
         $setup->endSetup();
